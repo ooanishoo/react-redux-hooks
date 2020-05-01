88a3a51901876fa9e3d3376fbe05c74ac95b9000
ooanishoo <anish.maharjan12341@gmail.com>
2020-05-01 15:21:56 +1000

    Fetching api key from env file

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ const INITIAL_STATE = {
 function giffReducer(state, { type, payload }) {
   switch (type) {
     case "load_giff":
-      console.log({payload});
+      console.log({ payload });
       return {
         ...state,
-        giffs: payload
-      }
+        giffs: payload,
+      };
 
     case "search_start":
       return {
@@ -34,14 +34,21 @@ function giffReducer(state, { type, payload }) {
 
 const fetchData = async (query) => {
   try {
+    const apiKey = process.env.REACT_APP_GIPHY_API_KEY;
     const response = await fetch(
-      `https://api.giphy.com/v1/gifs/search?api_key=w2yRw08pa8gLRyryPwXRzx0eAkDWqYbj&q=${query}&limit=10&offset=0&rating=G&lang=en`
+      `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${query}&limit=10&offset=0&rating=G&lang=en`
     );
 
     const json = await response.json();
     return json.data.map((item) => item.images.preview.mp4);
   } catch (error) {}
 };
+
+const useSetToLocalStorage = (giffs) => {
+  useEffect(() => {
+    localStorage.setItem("giffs", JSON.stringify(giffs));
+  }, [giffs]);
+}
 
 export default function App() {
   const [search, setSearch] = useState("");
@@ -53,22 +60,21 @@ export default function App() {
   const didReload = useRef(false);
 
   useEffect(() => {
-
-    if(!didReload.current){
-      console.log('running.....');
+    if (!didReload.current) {
       const data = JSON.parse(localStorage.getItem("giffs"));
-      console.log({data});
-      dispatch({ type: "load_giff", payload: data });
+      console.log({ data });
+      if (data !== null) dispatch({ type: "load_giff", payload: data });
       didReload.current = true;
     }
   }); // this useEffect runs only once
 
-  useEffect(() => {
-    localStorage.setItem("giffs", JSON.stringify(giffs));
-  }, [giffs]);
+  useSetToLocalStorage(giffs);
+
+  // useEffect(() => {
+  //   localStorage.setItem("giffs", JSON.stringify(giffs));
+  // }, [giffs]);
 
   // Create context objects
-  const DispatchContext = React.createContext();
 
   const searchGifs = (e) => {
     e.preventDefault();
@@ -77,6 +83,7 @@ export default function App() {
   };
 
   useEffect(() => {
+    console.log("query useEffect");
     if (query !== "") {
       fetchData(query).then((giffs) =>
         dispatch({
@@ -88,7 +95,7 @@ export default function App() {
   }, [query]);
 
   return (
-    <DispatchContext.Provider value={dispatch}>
+    <div>
       <h1>Async React hooks</h1>
       <form onSubmit={searchGifs}>
         <input
@@ -98,12 +105,15 @@ export default function App() {
         />
         <button type="submit">Search</button>
       </form>
+      {/* {giffs.map((giff) => (
+          <video key={giff} src={giff} />
+        ))} */}
 
       {isLoading ? (
         <h1>Searching...</h1>
       ) : (
         giffs.map((giff) => <video key={giff} src={giff} />)
       )}
-    </DispatchContext.Provider>
+    </div>
   );
 }
